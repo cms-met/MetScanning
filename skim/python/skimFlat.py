e9a58b6dc4591908a73cb7f738f34bca27a9b415
Constantin Heidegger <constantin.heidegger@cern.ch>
2015-08-21 10:32:44 +0200

    early 25ns stuff

--- skim/python/skimFlat.py
+++ skim/python/skimFlat.py
@@ -32,11 +32,11 @@ process.source = cms.Source(
     #fileNames = cms.untracked.vstring("root://eoscms.cern.ch//store/express/Run2015B/ExpressPhysics/FEVT/Express-v1/000/250/985/00000/04380D9C-0F24-E511-9772-02163E0127EF.root")
 
     #HOTLINE
-    fileNames = cms.untracked.vstring(
-      "root://eoscms.cern.ch//store/backfill/1/express/Tier0_Test_SUPERBUNNIES_vocms015/StreamExpress/ALCARECO/Hotline-Express-v28/000/251/642/00000/3693DCE3-962A-E511-A3DF-02163E011816.root",
+    #fileNames = cms.untracked.vstring(
+    #  "root://eoscms.cern.ch//store/backfill/1/express/Tier0_Test_SUPERBUNNIES_vocms015/StreamExpress/ALCARECO/Hotline-Express-v28/000/251/642/00000/3693DCE3-962A-E511-A3DF-02163E011816.root",
     #  "root://eoscms.cern.ch//store/backfill/1/express/Tier0_Test_SUPERBUNNIES_vocms015/StreamExpress/ALCARECO/Hotline-Express-v28/000/251/642/00000/AE854519-982A-E511-8AC8-02163E0138A8.root",
     #  "root://eoscms.cern.ch//store/backfill/1/express/Tier0_Test_SUPERBUNNIES_vocms015/StreamExpress/ALCARECO/Hotline-Express-v28/000/251/642/00000/C20EDBC4-962A-E511-BDBB-02163E01259F.root"
-    )
+    #)
 
     # Giulia's talk
    # fileNames = cms.untracked.vstring(
@@ -48,7 +48,61 @@ process.source = cms.Source(
    #   "file:private/JetHT_crystals.root"
    #)
 
-
+    ## ExpressPhysics 4T 25ns
+    fileNames = cms.untracked.vstring(
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/7A8F5522-C347-E511-A2F8-02163E014652.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/4C71411E-C347-E511-848B-02163E01354F.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/90CB99A3-C447-E511-BC2B-02163E0137EF.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/0CEFD0A4-C447-E511-ABF0-02163E0133EC.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/22CE3E48-C847-E511-8BF6-02163E016C4C.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/1AE46E23-C847-E511-81CB-02163E012A4C.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/5647846E-C847-E511-806A-02163E014549.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/E0CB6C1A-C647-E511-A5F5-02163E014103.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/862554B2-C947-E511-980B-02163E014103.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/AE593BC9-C947-E511-8949-02163E0133FE.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/16CC9222-C647-E511-8135-02163E0143DD.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/EE667CAD-CA47-E511-ABB8-02163E012AB5.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/98B6B30C-C747-E511-8FC7-02163E014222.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/3C29EFC0-CA47-E511-8EBE-02163E01354F.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/BC615EBC-CA47-E511-835C-02163E014348.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/7A2B1B20-C847-E511-BE37-02163E01202A.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/46B954BC-CA47-E511-A57B-02163E014348.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/C8DB54B5-CD47-E511-AD96-02163E014222.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/B6FBDAB7-CD47-E511-B681-02163E014376.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/5AC12C1D-C647-E511-BBCF-02163E01475A.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/76CB5B58-CC47-E511-8394-02163E014652.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/3607EA55-CC47-E511-A855-02163E014488.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/26E895BC-CA47-E511-B633-02163E01385E.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/481590B6-CD47-E511-BC0C-02163E014488.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/AC4554DF-CE47-E511-8D6F-02163E014466.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/C67F0EB4-CD47-E511-8817-02163E0143DD.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/5C0D19FC-CF47-E511-9A69-02163E014376.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/7A442DE2-D047-E511-9B26-02163E014466.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/2C7530F6-D147-E511-AFF4-02163E012AAF.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/16FA7D5D-CC47-E511-A27A-02163E01549A.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/A8DBD7E2-CE47-E511-9D4B-02163E011CBD.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/D8427AFD-CF47-E511-8737-02163E014652.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/0495D5E5-D147-E511-B20D-02163E01374D.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/56D2E878-D347-E511-9736-02163E012AAF.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/82AB35F9-CE47-E511-9C8C-02163E01559C.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/EED79768-D347-E511-B854-02163E011851.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/4E34D767-D347-E511-AC1E-02163E0133C6.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/B49A0AC1-D447-E511-BCAD-02163E014222.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/165C8DFF-D147-E511-9A00-02163E015529.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/7C105004-D247-E511-9398-02163E01549A.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/58344BFC-CF47-E511-ACED-02163E014488.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/98C8C2D3-D447-E511-9A0F-02163E014434.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/441DF867-D347-E511-AB00-02163E0133C6.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/A20E077D-D347-E511-9149-02163E012A4C.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/32F0E8F3-D147-E511-BA0A-02163E0143DD.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/CAEA07F7-D147-E511-82D6-02163E014752.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/F22F0F89-D547-E511-A7AE-02163E0134D1.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/EC33FA17-D647-E511-BE63-02163E011CBD.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/DAF2D016-D647-E511-BC8D-02163E0145A2.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/B6B2EF15-D647-E511-83E7-02163E014243.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/DE42A5F7-D147-E511-B301-02163E0137EF.root",
+    "root://eoscms.cern.ch//store/express/Run2015C/ExpressPhysics/FEVT/Express-v1/000/254/790/00000/E22F8518-D647-E511-ADC3-02163E012AAF.root"
+    )
     )
 
 
